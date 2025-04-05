280e9c28b023566dde42e6e870f25b5fc06bfdec
nethmalds <gddsnethmal@students.nsbm.ac.lk>
2025-04-05 12:24:46 +0530

    feat: create a new section for top level companies indide the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Services from "@/components/services-section";
 import Categories from "@/components/categories-section";
 import LatestJobSection from "@/components/latest-job-section";
 import TopFreelancers from "@/components/top-freelancers";
+import TopCompanies from "@/components/top-companies";
 export default function Home() {
 	return (
 		<>
@@ -12,6 +13,7 @@ export default function Home() {
 			<Categories />
 			<LatestJobSection />
 			<TopFreelancers />
+			<TopCompanies />
 		</>
 	);
 }
